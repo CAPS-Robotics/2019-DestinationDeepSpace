3896248bf3aa933d424847946ee3007ee9fc7830
Mingyoung Jeng <mingyoungjeng@gmail.com>
2018-10-05 12:14:12 -0500

    Updated OI

--- src/main/java/frc/team2410/robot/OI.java
+++ src/main/java/frc/team2410/robot/OI.java
@@ -3,16 +3,14 @@ package frc.team2410.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 
-public class OI
-{
+public class OI {
 	boolean[] canPress = new boolean[12];
+	boolean[] canPress1 = new boolean[12];
 	public Joystick joy1;
 	XboxController buttonPad;
 
-	public OI()
-	{
-		for (int i = 0; i < canPress.length; i++)
-		{
+	public OI() {
+		for (int i = 0; i < canPress.length; i++) {
 			canPress[i] = true;
 		}
 		joy1 = new Joystick(0);
@@ -20,121 +18,103 @@ public class OI
 	}
 
 	void pollButtons() {
-		if (joy1.getRawButton(2))
-		{
+		if (joy1.getRawButton(2)) {
 			Robot.drivetrain.returnWheelsToZero();
 		}
 
-		if (joy1.getRawButton(1))
-		{
-			if (canPress[0])
-			{
+		if (joy1.getRawButton(1)) {
+			if (canPress[0]) {
 				Robot.arm.toggleIntake();
 			}
 			canPress[0] = false;
 		}
-		else
-		{
+		else {
 			canPress[0] = true;
 		}
 
-		if (joy1.getRawButton(3))
-		{
-			if (canPress[2])
-			{
+		if (buttonPad.GetRawButton(11)) {
+			if (canPress1[10]) {
 				Robot.arm.toggleKick();
 			}
-			canPress[2] = false;
-		}
-		else
-		{
-			canPress[2] = true;
+			canPress1[10] = false;
+		} else {
+			canPress1[10] = true;
 		}
 
-		if (joy1.getRawButton(6))
-		{
-			Robot.gyro.resetHeading();
-		}
-
-		if (Math.abs(Robot.arm.getCurrent()) < 30)
-		{
-			if (this.getStick() == 0)
-			{
-				if (Math.abs(Robot.arm.cimcoder.getDistance() - Robot.arm.targetPos) < .5)
-				{
-					Robot.arm.armMotor.set(0);
-				}
-				else
-				{
-					Robot.arm.armMotor.set(this.getStick());
-					Robot.arm.targetPos = Robot.arm.cimcoder.getDistance();
-				}
+		if (buttonPad.GetRawButton(4)) {
+			if (canPress1[3]) {
+				Robot.drivetrain.Shift();
 			}
+			canPress1[3] = false;
+		} else {
+			canPress1[3] = true;
+		}
+
+		if(joy1.GetRawButton(6)) {
+			Robot.gyro.ResetHeading(0);
+		}
+
+		if (this.GetStick() == 0) {
+			if (Math.abs(Robot.arm.GetPosition() - Robot.arm.targetPos) < 1){
+				Robot.arm.armMotor.Set(0);
+			}
+		} else {
+			Robot.arm.armMotor.Set(-this.GetStick());
+			Robot.arm.targetPos = Robot.arm.GetPosition();
+		}
+		if (buttonPad.GetRawButton(1)) {
+			if(canPress1[0]) {
+				Robot.vision.SetCamera(1-Robot.vision.camera);
+				canPress1[0] = false;
+			}
+		} else {
+			canPress1[0] = true;
 		}
 
 		//Scale
-		if (buttonPad.getRawButton(4))
-		{
-			Robot.arm.moveTo(72);
-		}
-		if (buttonPad.getRawButton(3))
-		{
-			Robot.arm.moveTo(60);
-		}
-		if (buttonPad.getRawButton(2))
-		{
-			Robot.arm.moveTo(48);
+		if (buttonPad.getPOV(0) == 0) {
+			Robot.arm.moveTo(70);
 		}
 
 		//Intake
-		if (buttonPad.getPOV(0) == 180)
-		{
+		if (buttonPad.getPOV(0) == 180) {
 			Robot.arm.moveTo(0);
 		}
 
 		//Switch
-		if (buttonPad.getPOV(0) == 90)
-		{
-			Robot.arm.moveTo(20);
+		if (buttonPad.getPOV(0) == 90) {
+			Robot.arm.moveTo(28);
 		}
 
 		//Reset
-		if (buttonPad.getPOV(0) == 270)
-		{
-			Robot.arm.cimcoder.reset();
+		if (buttonPad.getPOV(0) == 270) {
+			Robot.arm.setPosition(0);
 			Robot.arm.targetPos = 0;
 		}
 	}
 
-	public double getX()
-	{
+	public double getX() {
 		return this.applyDeadzone(joy1.getRawAxis(0), 0.15, 1);
 	}
 
-	public double getY()
-	{
+	public double getY() {
 		return this.applyDeadzone(-joy1.getRawAxis(1), 0.15, 1);
 	}
 
-	public double getTwist()
-	{
+	public double getTwist() {
 		return this.applyDeadzone(joy1.getRawAxis(2), 0.70, 1) / 2;
 	}
 
-	public double getSlider()
-	{
+	public double getSlider() {
 		return joy1.getRawAxis(3);
 	}
 
-	double getAnalogY(int stickNum)
-	{
+	double getAnalogY(int stickNum){
 		return this.applyDeadzone(buttonPad.getRawAxis(stickNum * 2 + 1), 0.50, 1);
 	}
 
-	double applyDeadzone(double val, double deadzone, double maxval)
-	{
-		if (Math.abs(val) <= deadzone)
-		{
+	double applyDeadzone(double val, double deadzone, double maxval) {
+		if (Math.abs(val) <= deadzone) {
 			return 0;
 		}
 
@@ -143,8 +123,7 @@ public class OI
 		return val;
 	}
 
-	double getStick()
-	{
+	double getStick() {
 		return this.applyDeadzone(buttonPad.getRawAxis(3), 0.50, 1);
 	}
 }
