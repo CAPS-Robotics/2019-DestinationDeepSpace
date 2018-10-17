0a19a12de60649601223d00e5a0d227af9e11879
HiItsMeHaHa <wherron01@gmail.com>
2018-10-17 14:40:41 -0500

    nuked auto trash

--- src/main/java/frc/team2410/robot/Subsystems/Autonomous.java
+++ src/main/java/frc/team2410/robot/Subsystems/Autonomous.java
@@ -42,94 +42,20 @@ public class Autonomous
 		SmartDashboard.putNumber("Auto State", state);
 		switch(autoNum) {
 			case 0:
-				//Same side switch
+				//Middle god auto
 				halfWay(this.swLeft);
 				break;
 			case 1:
-				//In the middle
+				//switch or scale on same side
 				sideAuto(this.roLeft);
 				break;
 			case 2:
-				//Opposite side switch (probably don't do this one usually unless you want to crash into someone and break everything)
+				//does the scale in auto
 				scaleAuto(this.roLeft);
 				break;
 		}
 	}
-
-	/*void crossField(boolean left) {
-		switch(state) {
-			case 0:
-				//lift arm to switch height
-				timer.reset();
-				timer.start();
-				Robot.arm.moveTo(22);
-				state++;
-				break;
-			case 1:
-				//Move across the field to the opposite side switch vision target
-				Robot.drivetrain.crabDrive(left ? -1 : 1, .2, 0, 1, false);
-				SmartDashboard.putNumber("time", timer.get());
-				if(Robot.vision.getCentralValue() > 120 + (left ? -80 : 80) && Robot.vision.getCentralValue() < 200 + (left ? -80 : 80)) { state++; }
-				//We've gone too far.  Abort.
-				if(timer.get() > 1.5) state = -1;
-				break;
-			case 2:
-				//Go fast straight until it's close enough
-				Robot.drivetrain.crabDrive(0, 1, 0, .75, false);
-				if(Robot.drivetrain.getDistanceAway() < 42) state++;
-				break;
-			case 3:
-				//Go slow straight until it's even closer
-				Robot.drivetrain.crabDrive(0, 1, 0, .25, false);
-				if(Robot.drivetrain.getDistanceAway() < 12) state++;
-				break;
-			default:
-				//Go around the switch
-				this.goAround(left);
-				break;
-			case -1:
-				Robot.drivetrain.brake();
-				break;
-		}
-	}*/
-
-	/*void straightAhead(boolean left) {
-		switch(state) {
-			case 0:
-				//Move arm to switch height
-				timer.reset();
-				timer.start();
-				Robot.arm.moveTo(22);
-				state++;
-				break;
-			case 1:
-				//Move forwardish to same side switch vision target
-				Robot.drivetrain.crabDrive(left ? .2 : -.2, 1, 0, 1, false);
-				SmartDashboard.putNumber("time", timer.get());
-				if(Robot.vision.getCentralValue() > 120 + (left ? -80 : 80) && Robot.vision.getCentralValue() < 200 + (left ? -80 : 80)) { state++; }
-				//Too far
-				if(timer.get() > 1.5) { state = -1; }
-				break;
-			case 2:
-				//Go fast straight until it's close enough
-				Robot.drivetrain.crabDrive(0, 1, 0, .75, false);
-				if(Robot.drivetrain.getDistanceAway() < 42) state++;
-				break;
-			case 3:
-				//Go slower straight until it's closer
-				Robot.drivetrain.crabDrive(0, 1, 0, .25, false);
-				if(Robot.drivetrain.getDistanceAway() < 12) state++;
-				break;
-			default:
-				//Go around switch
-				this.goAround(left);
-				break;
-			case -1:
-				Robot.drivetrain.brake();
-				break;
-		}
-	}*/
-
+	
 	void halfWay(boolean left) {
 		switch(state) {
 			case -1:
@@ -185,161 +111,7 @@ public class Autonomous
 				break;
 		}
 	}
-
-	/*void goAround(boolean left) {
-		switch(state) {
-			case 4:
-				//Go sideways until you've passed the switch
-				Robot.drivetrain.crabDrive(left ? -1 : 1, 0 , 0, .5, false);
-				if(Robot.drivetrain.getDistanceAway() > 50) {
-					Robot.drivetrain.startTravel();
-					state++;
-				}
-				break;
-			case 5:
-				//Go sideways another 24 inches
-				Robot.drivetrain.crabDrive(left ? -1 : 1, 0 , 0, .5, false);
-				if(Math.abs(Robot.drivetrain.getTravel()) >= 24 + (left ? RobotMap.SONAR_CENTER : -RobotMap.SONAR_CENTER)) {
-					Robot.drivetrain.startTravel();
-					state++;
-				}
-				break;
-			case 6:
-				//Go forwards 100 inches
-				Robot.drivetrain.crabDrive(0, 1, 0, .5, false);
-				if(Math.abs(Robot.drivetrain.getTravel()) >= 100) {
-					Robot.drivetrain.startTravel();
-					state++;
-				}
-				break;
-			case 7:
-				//Sideways back to switch
-				Robot.drivetrain.crabDrive(left ? -1 : 1, 0, 0, .5, false);
-				if(Math.abs(Robot.drivetrain.getTravel()) >= 30.5) state++;
-			case 8:
-				//Turn 180
-				Robot.drivetrain.crabDrive(0, 0, Robot.gyro.getHeading() > 0 ? 1 : -1, 0.5, false);
-				if(Math.abs(Robot.gyro.getHeading() - 180) < 5) {
-					Robot.arm.open();
-					Robot.arm.kickDown();
-					state++;
-				}
-				break;
-			case 9:
-				//Grab a new cube
-				Robot.arm.open();
-				Robot.drivetrain.crabDrive(0, -1, 0, 0.25, false);
-				if(Robot.drivetrain.getDistanceAway() < 2) {
-					Robot.arm.close();
-					state++;
-				}
-				break;
-			default:
-				//Scale auto *magic*
-				if(autoSc)
-					scaleAhead(scLeft);
-				else
-					scaleAcross(scLeft);
-				break;
-			case -1:
-				Robot.drivetrain.brake();
-				break;
-		}
-	}*/
-
-	/*void scaleAhead(boolean left) {
-		switch(state) {
-			case 10:
-				//lift arm to talll
-				Robot.arm.moveTo(72);
-				state++;
-				break;
-			case 11:
-				//turn 180
-				Robot.drivetrain.crabDrive(0, 0, Robot.gyro.getHeading() < 0 ? 1 : -1, 0.5, false);
-				if(Math.abs(Robot.gyro.getHeading()) < 5) {
-					Robot.drivetrain.startTravel();
-					state++;
-				}
-				break;
-			case 12:
-				//go sideways to the scale
-				Robot.drivetrain.crabDrive(scLeft ? -1 : 1, 0, 0, .5, false);
-				if(Math.abs(Robot.drivetrain.getTravel()) >= 18) {
-					Robot.drivetrain.startTravel();
-					Robot.arm.kickUp();
-					state++;
-				}
-				break;
-			case 13:
-				//go forward to the scale and drop
-				Robot.drivetrain.crabDrive(0, 1, 0, .75, false);
-				if(Math.abs(Robot.drivetrain.getTravel()) >= 65) {
-					Robot.arm.open();
-					Robot.arm.kickDown();
-					Robot.drivetrain.startTravel();
-					state++;
-				}
-				break;
-			case 14:
-				//back off scale and lower arm
-				Robot.drivetrain.crabDrive(0, -1, 0, .5, false);
-				if(Math.abs(Robot.drivetrain.getTravel()) >= 21) {
-					Robot.arm.moveTo(0);
-					state++;
-				}
-			default:
-				Robot.drivetrain.brake();
-				break;
-		}
-	}*/
-
-	/*void scaleAcross(boolean left) {
-		switch(state) {
-			case 10:
-				//arm up
-				Robot.arm.moveTo(72);
-				state++;
-				break;
-			case 11:
-				//turn around
-				Robot.drivetrain.crabDrive(0, 0, Robot.gyro.getHeading() < 0 ? 1 : -1, 0.5, false);
-				if(Math.abs(Robot.gyro.getHeading()) < 5) {
-					Robot.drivetrain.startTravel();
-					state++;
-				}
-				break;
-			case 12:
-				//go to the other scale
-				Robot.drivetrain.crabDrive(scLeft ? 1 : -1, 0, 0, .5, false);
-				if(Math.abs(Robot.drivetrain.getTravel()) >= 159) {
-					Robot.drivetrain.startTravel();
-					Robot.arm.kickUp();
-					state++;
-				}
-				break;
-			case 13:
-				//go forward and drop
-				Robot.drivetrain.crabDrive(0, 1, 0, .75, false);
-				if(Math.abs(Robot.drivetrain.getTravel()) >= 65) {
-					Robot.arm.open();
-					Robot.arm.kickDown();
-					Robot.drivetrain.startTravel();
-					state++;
-				}
-				break;
-			case 14:
-				//back up and lower
-				Robot.drivetrain.crabDrive(0, -1, 0, .5, false);
-				if(Math.abs(Robot.drivetrain.getTravel()) >= 21) {
-					Robot.arm.moveTo(0);
-					state++;
-				}
-			default:
-				Robot.drivetrain.brake();
-				break;
-		}
-	}*/
+	
 	void sideAuto(boolean left) {
 		switch(state) {
 			case 0:
