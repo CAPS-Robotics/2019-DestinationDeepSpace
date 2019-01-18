0ac0e27aa45f739853d98f90c983ab8d79180354
HiItsMeHaHa <wherron01@gmail.com>
2019-01-17 19:43:35 -0600

    Adding Talon Pair Class

--- src/main/java/frc/team2410/robot/Subsystems/Intake.java
+++ src/main/java/frc/team2410/robot/Subsystems/Intake.java
@@ -1,32 +1,28 @@
 package frc.team2410.robot.Subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import frc.team2410.robot.TalonPair;
+
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 
 import static frc.team2410.robot.RobotMap.*;
 
 class Intake {
-	private WPI_TalonSRX wheelMotorTop;
-	private WPI_TalonSRX wheelMotorBottom;
+	TalonPair talonPair;
 	private DoubleSolenoid solenoid;
 	private boolean open = false;
 	
 	Intake() {
-		wheelMotorTop = new WPI_TalonSRX(INTAKE_MOTOR_BOTTOM);
-		wheelMotorBottom = new WPI_TalonSRX(INTAKE_MOTOR_TOP);
+		talonPair = new TalonPair(INTAKE_MOTOR_TOP, INTAKE_MOTOR_BOTTOM, false, false);
 		solenoid = new DoubleSolenoid(HATCH_INTAKE_FORWARD, HATCH_INTAKE_REVERSE, PCM);
 	}
 	
 	void setWheel(boolean in) {
-		int speed = open ? 1 : -1;
-		wheelMotorTop.set(speed);
-		wheelMotorBottom.set(-speed);
+		talonPair.set(open ? 1 : -1);
 	}
 	
 	void stop() {
-		wheelMotorTop.set(0);
-		wheelMotorBottom.set(0);
+		talonPair.set(0);
 	}
 	
 	void togglePiston() {
